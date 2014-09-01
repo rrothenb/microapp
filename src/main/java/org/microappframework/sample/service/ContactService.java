ba3dc7185579ca3e9a7606195c5a61b1f6368b57
Rick Rothenberg <rick.rothenberg@readytalk.com>
2014-09-01 11:38:07 -0600

    stuff

--- src/main/java/org/microappframework/sample/service/ContactService.java
+++ src/main/java/org/microappframework/sample/service/ContactService.java
@@ -1,19 +1,30 @@
 package org.microappframework.sample.service;
 
+import org.springframework.data.repository.query.Param;
 import org.microappframework.sample.data.access.Contact;
 import org.microappframework.sample.data.access.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 public class ContactService {
     @Autowired
     ContactRepository contactRepository;
 
+    @RequestMapping("/")
+    public String thingy() {
+        return "Hello!";
+    }
+/*
     @Transactional
-    void createContact(String name) {
+    @RequestMapping("/contact/create")
+    void createContact(@Param("name") String name) {
         Contact contact = new Contact();
+        contact.setName(name);
         contactRepository.save(contact);
     }
+    */
 }
