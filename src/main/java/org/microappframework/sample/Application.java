5d34309dd90d1ed8056637dc81ce42559112d1eb
Rick Rothenberg <rrothenb@yahoo.com>
2014-09-01 17:51:05 -0600

    more

--- src/main/java/org/microappframework/sample/Application.java
+++ src/main/java/org/microappframework/sample/Application.java
@@ -2,8 +2,11 @@ package org.microappframework.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) throws Exception {
--- src/main/java/org/microappframework/sample/service/ContactService.java
+++ src/main/java/org/microappframework/sample/service/ContactService.java
@@ -1,30 +1,24 @@
 package org.microappframework.sample.service;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.data.repository.query.Param;
 import org.microappframework.sample.data.access.Contact;
 import org.microappframework.sample.data.access.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j
 @RestController
 public class ContactService {
     @Autowired
     ContactRepository contactRepository;
 
-    @RequestMapping("/")
-    public String thingy() {
-        return "Hello!";
-    }
-/*
-    @Transactional
     @RequestMapping("/contact/create")
     void createContact(@Param("name") String name) {
         Contact contact = new Contact();
         contact.setName(name);
-        contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        log.info(contact);
     }
-    */
 }
